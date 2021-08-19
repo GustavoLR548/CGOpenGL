f328cb0fe6b0f3e61935c530a35c61c346768843
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-08-18 23:10:28 -0300

    added some commentary

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int main(void)
 
     glfwMakeContextCurrent(window);
 
+    //Setting up a window with a blue navy color
     gladLoadGL();
     glViewport(0,0,800,800);
     glClearColor(0.07f,0.13f,0.17f,1.0f);
