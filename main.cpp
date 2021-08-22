6d8daaf25d24a4657579e6157dd2d4f595255a68
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-08-22 13:33:06 -0300

    feat: added commentary for better clarity

--- main.cpp
+++ main.cpp
@@ -7,13 +7,21 @@ int main(void)
 {
     GLFWwindow* window;
     
+    //Trying to initialize the program
     if(!glfwInit()) 
         return -1;
     
+    //Setting up the version of glfw to be
+    //only the version 3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    //Setting up the core profile of opengl, for having access
+    //with the new functionality
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    //Creating and setting up the window, and checking if 
+    //it was created properly
     window = glfwCreateWindow(800, 800, "Test", NULL, NULL);
     if(window == NULL) {
 
@@ -22,26 +30,37 @@ int main(void)
         return -1;
     }
 
+    //OpenGL will keep looking at the current
+    //window that we have created
     glfwMakeContextCurrent(window);
 
     //Setting up a window with a blue navy color
-    gladLoadGL();
-    glViewport(0,0,800,800);
-    glClearColor(0.07f,0.13f,0.17f,1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
+    gladLoadGL();                           //load necessary configurations
+    glViewport(0,0,800,800);                //Area of the window that OpenGL will act 
+    glClearColor(1.0f,0.64f,0.00f,1.0f);    //Setting up the aRGB of the color (Red, green, blue, alpha)
+    glClear(GL_COLOR_BUFFER_BIT);           //Clean the back buffer and assign the new color to it
+    glfwSwapBuffers(window);                //Swap the back buffer with the front buffer
 
+    //Run the window until it is closed
+    //ex: Click the 'x' or a function tells the
+    //window to close
     while(!glfwWindowShouldClose(window)) {
 
-        double x,y;
-        glfwPollEvents();
-        glfwGetCursorPos(window,&x,&y);
+        double mouse_x, mouse_y;
+        glfwPollEvents();  //Process all openGL events
+
+        //Get the current cursor position
+        glfwGetCursorPos(window,&mouse_x,&mouse_y); 
+        
+        //Verify if the mouse 1 button was pressed
         if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS) {
-            std::cout<< x << " ; " << y << std::endl;
-            sleep(1);
+            std::cout<< mouse_x << " ; " << mouse_y << std::endl;
+            sleep(1); //making the program sleep so that it only activate it once
         }
     }
-
+    
+    //Destroy and terminate the program
+    //(Necessary funcitons to call!)
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
